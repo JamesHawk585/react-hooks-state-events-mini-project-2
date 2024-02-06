2e5c3e4605a6f9d850edc0e283f373cb93a502d8
James Hawk <jph94880@gmail.com>
2024-02-06 08:58:53 -0600

    Adds CategoryFilter.jsx, NewTaskForm.jsx, and TaskList.jsx.

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+import TaskList from "./TaskList";
+import NewTaskForm from "./NewTaskForm";
+import CategoryFilter from "./CategoryFilter";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>My Tasks</h2>
+      <CategoryFilter />
+      <NewTaskForm />
+      <TaskList />
+    </div>
+  );
+}
+
+export default App;
